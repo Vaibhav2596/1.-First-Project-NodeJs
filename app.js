51163241b755131f71f1317a5a3f384f38be4c99
Vaibhav2596 <chavanv214@gmail.com>
2026-05-04 15:55:07 +0530

    Code

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ var name = "Namaste NodeJS";
 var a = 10;
 var b = 20;
 
-console.log(name);
-console.log(a + b);
+// console.log(name);
+// console.log(a + b); 
+
+// console.log(global);
+console.log(this);  //Empty Object
+
+console.log(globalThis);
+console.log(globalThis === global);
