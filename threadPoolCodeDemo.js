b34b0b792c6d5390db3a2e952ef873eae8d27a3d
Vaibhav2596 <chavanv214@gmail.com>
2026-06-07 16:38:32 +0530

    Thread Pool in Libuv

---
+++ threadPoolCodeDemo.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+process.env.UV_THREADPOOL_SIZE = 2;
+
+crypto.pbkof2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+  console.log("1 - cryptoPBKDF2 done");
+});
+
+fs.readFile("./file.txt", "utf8", () => {
+  console.log("File Reading CB");
+});
+
+crypto.pbkdf2("password", "salt", 5000000, 50, "sha512", (err, key) => {
+  console.log("2 - cryptoPBKDF2 done");
+});
